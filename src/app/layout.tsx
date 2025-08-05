8aaf759e3e1db119df52209602898213c6b848ea
Hanzlah Javed <hanzlahjaved101@gmail.com>
2025-08-05 20:43:56 +0500

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 
 import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/sonner"
 import { Navbar } from "@/components/navbar"
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
